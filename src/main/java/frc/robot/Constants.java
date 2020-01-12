74e68d7de252fe5e2a81fc738474d71c8895eb5d
ChristianR839 <46659572+ChristianR839@users.noreply.github.com>
2020-01-12 13:34:35 -0500

    initial commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,37 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
+ *
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+    public static int TalonSRX01ID = 1;
+    public static int TalonSRX02ID = 2;
+    public static int TalonSRX03ID = 3;
+    public static int TalonSRX04ID = 4;
+    public static int TalonSRX05ID = 5;
+    public static int TalonSRX06ID = 6;
+
+    public static WPI_TalonSRX leftFront = new WPI_TalonSRX(TalonSRX02ID);
+    public static WPI_TalonSRX leftFollower = new WPI_TalonSRX(TalonSRX01ID);
+    public static WPI_TalonSRX rightFront = new WPI_TalonSRX(TalonSRX06ID);
+    public static WPI_TalonSRX rightFollower = new WPI_TalonSRX(TalonSRX05ID);
+    public static WPI_TalonSRX intakeMotor = new WPI_TalonSRX(TalonSRX03ID);
+    public static WPI_TalonSRX intakeFollowerTemp = new WPI_TalonSRX(TalonSRX04ID);
+
+}
---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,67 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.XboxController.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import frc.robot.commands.TankDrive;
+import frc.robot.commands.intakeInCommand;
+import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.Intake;
+import edu.wpi.first.wpilibj2.command.Command;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a "declarative" paradigm, very little robot logic should
+ * actually be handled in the {@link Robot} periodic methods (other than the
+ * scheduler calls). Instead, the structure of the robot (including subsystems,
+ * commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final DriveTrain driveTrain = new DriveTrain();
+  private final Intake intake = new Intake();
+
+  public UniversalJoystick joystick = new UniversalJoystick(0);
+
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer() {
+    // Configure the button bindings
+
+    driveTrain.setDefaultCommand(
+        new TankDrive(() -> joystick.getY(Hand.kLeft), () -> joystick.getY(Hand.kRight), driveTrain));
+
+    configureButtonBindings();
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be
+   * created by instantiating a {@link GenericHID} or one of its subclasses
+   * ({@link edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then
+   * passing it to a {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+    final JoystickButton aJoystickButton = new JoystickButton(joystick, 1);
+
+    aJoystickButton.whenPressed( new intakeInCommand(intake));
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An DriveWithJoystick will run in autonomous
+    return null;
+  }
+}
