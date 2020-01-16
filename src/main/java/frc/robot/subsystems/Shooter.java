c68818e906f30bc4ad666bb5211ef370862c5285
PhilipFini <58013918+PhilipFini@users.noreply.github.com>
2020-01-15 19:35:57 -0500

    Edited Shooter

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2019-2020 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -12,9 +12,36 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
-public class Shooter extends SubsystemBase {
+ public class Shooter extends Subsystem 
+ {
 
- public Shooter() {
+//unsure on what to change initDefaultCommand, setDefaultCommand, Forward, Reverse, and Stop into.
+    public Shooter() {
 
+     SpeedController shooterMotor = Constants.shooterMotor;
+     
+     // Put methods for controlling this subsystem
+     // here. Call these from Commands.
+     public void initDefaultCommand() 
+     {
+         // Set the default command for a subsystem here.
+         setDefaultCommand(new Shooter());
+     }
+     
+     public void Forward() {
+     {
+        shooterMotor.set( 1 );
+     }
+     
+     public void Reverse()
+     {
+        shooterMotor.set( -1 );
+     }
+     
+     public void Stop()
+     {
+        shooterMotor.set( 0 );
+     }
+    }
  }
 }
